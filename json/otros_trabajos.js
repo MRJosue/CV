ab282e91ade31c86a74d7672fe4777087328b11f
josue.cardona <ingjosue.cardona@gmail.com>
2022-09-13 14:20:04 -0500

    actualizacion contenido && trabajos

---
+++ json/otros_trabajos.js
@@ -0,0 +1,13 @@
+// ,
+//     {
+//         "Empresa":"Power Hit S.A. de C.V",
+//         "tiempo":"Diciembre 2018 - Julio 2019",
+//         "Puesto":"Técnico: Desarrollador PHP ",
+//         "desc":"Diseño y modelado de diagramas uml <br/> diseño y mantenimiento de sitemas web de administracion "
+//     },
+//     {
+//         "Empresa":"SIAD S.A. de C.V",
+//         "tiempo":"Enero 2018 - Diciembre 2018",
+//         "Puesto":"Programador JR",
+//         "desc":"Diseño y desarrollo de sitios web, diseño de diseños industriales, soporte técnico y programas de respaldo."
+//     }
